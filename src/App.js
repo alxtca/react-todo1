8a4e4647ce923f035561c7bf5a548db962f11b36
Alexandr Tcacenco <alxtca@gmail.com>
2022-01-13 10:03:08 +0100

    added local storage, checkbox

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function App() {
   const initialValue = [{id: 0, task: "Hello", complete: false}, {id: 1, task: "World", complete: true}]  
   const [userInput, setUserInput] = useState()
   const [taskList, setTaskList] = useState(initialValue)
+
+  // load task list if exist
+  useEffect(() => {
+    if (localStorage.getItem("react-todo")) {
+      setTaskList(JSON.parse(localStorage.getItem("react-todo")))
+    }
+  }, [])
+  // update task list in storage on state change
+  useEffect(() => {
+    localStorage.setItem("react-todo", JSON.stringify(taskList))
+  }, [taskList])
    
   function handleSubmit(e) {
     e.preventDefault()
@@ -20,20 +31,54 @@ function App() {
   
   return (
   <>
-    Form:
-      <form onSubmit={handleSubmit}>
-        <input value={userInput} onChange={e => setUserInput(e.target.value)} placeholder="Enter text here..."/>
-        <button type="submit">Submit</button>
-      </form>
-      <ul>
-        {taskList.map((task, index) => <li className={ task.complete ? "strike" : "" } id={task.id}> {task.task}
-                            <button onClick={() => handleStrike(index)}>Strike</button>
-                            <button onClick={() => handleDelete(task.id)}>Delete</button>
-                            </li>)}        
-      </ul>
-      
+    <ul>
+      {taskList.map((task, index) =>
+       <ListItem
+        handleDelete={handleDelete}
+        handleStrike={handleStrike}
+        task={task}
+        index={index}
+        />          )
+      }        
+    </ul>
+    <MyForm handleSubmit={handleSubmit} userInput={userInput} setUserInput={setUserInput}/>   
   </>
   )
 }
+
+function MyForm(props) {
+  return (
+    <>
+      <form onSubmit={props.handleSubmit}>        
+        <input value={props.userInput} onChange={e => props.setUserInput(e.target.value)} placeholder="Enter text here..."/>
+        <button type="submit">Add Task</button>
+      </form>
+    </>
+  )
+}
+
+function ListItem(props) {
+
+  return (
+    <>
+      <li className={ props.task.complete ? "strike" : "" }
+          id={props.task.id}>
+            <Checkbox task={props.task} handleStrike={props.handleStrike} index={props.index}/>
+            {props.task.task}            
+            <button onClick={() => props.handleDelete(props.task.id)}>Delete</button>
+      </li>
+    </>
+  )
+}
+
+const Checkbox = (props) => {
+  return (
+    <input
+    type="checkbox"
+    checked={props.task.complete ? true : false}
+    onChange={() => props.handleStrike(props.index)} />
+  )
+}
+
 
 export default App;
